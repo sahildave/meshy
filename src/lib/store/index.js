7e491292f6f8b2bb088946419d867227a4b2920b
Sahil Dave <6861255+sahildave@users.noreply.github.com>
2025-04-10 10:50:46 +0200

    Update index.js

--- src/lib/store/index.js
+++ src/lib/store/index.js
@@ -9,10 +9,10 @@ export const meshId = writable(757);
       --gradient-color-4: #c92f3c;
 */
 export const inputColors = writable([
-  "#eb75b6",
-  "#ddf3ff",
-  "#6e3deb",
-  "#c92f3c",
+  "#000000",
+  "#11003D",
+  "#34007A",
+  "#ba9eff",
 ]);
 
 /*----------------------------------------------------------------
